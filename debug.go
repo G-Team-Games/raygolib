8bc5e8e2cc4e693c36210f3642e674d71f9aad15
Tomasz Petrykowski <t.petrykowski.069@studms.ug.edu.pl>
2026-04-11 19:50:34 +0200

    Refactor debug mode - add config, custom rl backend and global instance

--- debug.go
+++ debug.go
@@ -1,19 +1,40 @@
 package raygolib
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	"image/color"
 
+	irl "github.com/G-Team-Games/raygolib/internal/raylib"
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
 type DebugAware interface {
 	SetDebug(*DebugAPI)
 }
 
+type DebugConfig struct {
+	StartEnabled bool
+	ToggleKey    int32 // Raylib constant type for keys is int32
+	ShowFPS      bool
+}
+
+func defaultDebugConfig() DebugConfig {
+	return DebugConfig{
+		StartEnabled: false,
+		ToggleKey:    rl.KeyF1,
+		ShowFPS:      true,
+	}
+}
+
 type debugWrapper struct {
-	next  Game
-	debug *DebugAPI
+	next      Game
+	debug     *DebugAPI
+	toggleKey int32
+	showFPS   bool
+	backend   irl.DebugBackend
 }
 
 func (d *debugWrapper) Update(dt float32) error {
-	if rl.IsKeyPressed(rl.KeyF1) {
+	if d.toggleKey != 0 && d.backend.IsKeyPressed(d.toggleKey) {
 		d.debug.Toggle()
 	}
 
@@ -28,23 +49,37 @@ func (d *debugWrapper) Draw() {
 		return
 	}
 
-	rl.DrawFPS(10, 10)
+	if d.showFPS {
+		d.backend.DrawFPS(10, 10)
+	}
 
 	d.debug.Flush()
 }
 
+// DEBUG API --------------------------------------------------------------- //
+
 type DebugAPI struct {
 	enabled bool
 	draws   []func()
+	backend irl.DebugBackend
 }
 
-func (d *DebugAPI) Rect(x, y, w, h float32) {
+var debugInstance *DebugAPI // nil until middleware is applied
+
+func Debug() *DebugAPI {
+	if debugInstance == nil {
+		panic("Apply debug middleware first to use debug API")
+	}
+	return debugInstance
+}
+
+func (d *DebugAPI) Rect(x, y, w, h float32, color color.RGBA) {
 	if !d.enabled {
 		return
 	}
 
 	d.draws = append(d.draws, func() {
-		rl.DrawRectangle(int32(x), int32(y), int32(w), int32(h), rl.Red)
+		d.backend.DrawRectangle(int32(x), int32(y), int32(w), int32(h), color)
 	})
 }
 
@@ -60,18 +95,29 @@ func (d *DebugAPI) Toggle() {
 }
 
 func DebugMiddleware() Middleware {
+	return DebugMiddlewareWithConfig(defaultDebugConfig())
+}
+
+func DebugMiddlewareWithConfig(cfg DebugConfig) Middleware {
 	return func(next Game) Game {
+		debugBackend := irl.NewRlDebugBackend()
+
 		debug := &DebugAPI{
-			enabled: true,
+			enabled: cfg.StartEnabled,
+			backend: debugBackend,
 		}
+		debugInstance = debug
 
 		if g, ok := next.(DebugAware); ok {
 			g.SetDebug(debug)
 		}
 
 		return &debugWrapper{
-			next:  next,
-			debug: debug,
+			next:      next,
+			debug:     debug,
+			toggleKey: cfg.ToggleKey,
+			showFPS:   cfg.ShowFPS,
+			backend:   debugBackend,
 		}
 	}
 }
