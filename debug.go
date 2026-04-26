82b52cb2cf51797d30c9ceee84a73dd22574e811
Tomasz Petrykowski <t.petrykowski.069@studms.ug.edu.pl>
2026-04-26 23:31:48 +0200

    Add logger to debug mode with custom TraceLogCallback function

--- debug.go
+++ debug.go
@@ -2,10 +2,56 @@ package raygolib
 
 import (
 	"image/color"
+	"time"
 
 	irl "github.com/G-Team-Games/raygolib/internal/raylib"
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
+
+const (
+	ansiReset   = "\033[0m"
+	ansiRed     = "\033[1;31m"
+	ansiYellow  = "\033[0;93m"
+	ansiMagenta = "\033[1;35m"
+	ansiCyan    = "\033[0;96m"
+	ansiWhite   = "\033[0;15m"
+)
+
+var traceLogLevelColors = map[rl.TraceLogLevel]string{
+	rl.LogAll:     ansiReset,
+	rl.LogDebug:   ansiCyan,
+	rl.LogInfo:    ansiWhite,
+	rl.LogWarning: ansiYellow,
+	rl.LogError:   ansiRed,
+	rl.LogFatal:   ansiMagenta,
+	rl.LogNone:    ansiReset,
+}
+
+var traceLogLevelNames = map[rl.TraceLogLevel]string{
+	rl.LogAll:     "ALL",
+	rl.LogDebug:   "DEBUG",
+	rl.LogInfo:    "INFO",
+	rl.LogWarning: "WARN",
+	rl.LogError:   "ERROR",
+	rl.LogFatal:   "FATAL",
+	rl.LogNone:    "NONE",
+}
+
+func TraceLogCallback(logLevel int, text string) {
+	color := traceLogLevelColors[rl.TraceLogLevel((logLevel))]
+	if color == "" {
+		color = ansiReset
+	}
+	levelName := traceLogLevelNames[rl.TraceLogLevel(logLevel)]
+	if levelName == "" {
+		levelName = "LOG"
+	}
+
+	now := time.Now()
+	timestamp := now.Format("15:04:05.000")
+
+	println(color + "[" + timestamp + "] [" + levelName + "]\t" + text + ansiReset)
+}
 
 type DebugAware interface {
 	SetDebug(*DebugAPI)
@@ -50,6 +96,12 @@ func (d *debugWrapper) Unwrap() Game {
 }
 
 func (d *debugWrapper) Update(dt float32) error {
+	if d.debug.enabled {
+		rl.SetTraceLogLevel(rl.LogDebug)
+	} else {
+		rl.SetTraceLogLevel(rl.LogInfo) // default for raylib
+	}
+
 	if d.toggleKey != 0 && d.backend.IsKeyPressed(d.toggleKey) {
 		d.debug.Toggle()
 	}
@@ -127,6 +179,10 @@ func DebugMiddlewareWithConfig(cfg DebugConfig) Middleware {
 			backend: debugBackend,
 		}
 		debugInstance = debug
+
+		rl.SetTraceLogCallback(TraceLogCallback)
+		rl.SetTraceLogLevel(rl.LogDebug)
+		rl.TraceLog(rl.LogDebug, "DEBUG MODE IS ON")
 
 		// Walk the middleware chain to find the DebugAware implementation
 		current := next
